d13e27b6b9a1381ef7a15b1d2495cb0b005d0874
joe kabucho <32999243+joekabucho@users.noreply.github.com>
2019-01-16 17:08:23 +0300

    Add files via upload

---
+++ RE_CamSwap.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var cam1 : Camera;
+var cam2 : Camera;
+
+private var walkedIn : boolean = false;
+
+function Start()
+{
+	cam1.camera.enabled = true;
+	cam2.camera.enabled = false;
+}
+
+function OnTriggerEnter(Col : Collider)
+{
+	if(Col.tag == "Player")
+	{
+		walkedIn = !walkedIn;
+	}
+}
+
+function Update()
+{
+	if(walkedIn == true)
+	{
+		cam1.camera.enabled = false;
+		cam2.camera.enabled = true;
+	}
+	
+	if(walkedIn == false)
+	{
+		cam1.camera.enabled = true;
+		cam2.camera.enabled = false;
+	}
+}
